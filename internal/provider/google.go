dd83b09ac5adeb1624756a753c11c0624a152637
Nick Maltsev <public.nmlc@gmail.com>
2023-06-22 16:49:53 +0700

    fixup! [gha] build & push to registry

--- internal/provider/google.go
+++ internal/provider/google.go
@@ -32,7 +32,7 @@ func (g *Google) Setup() error {
 	}
 
 	// Set static values
-	g.Scope = "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/admin.directory.group.member.readonly"
+	g.Scope = "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/admin.directory.group.readonly"
 	g.LoginURL = &url.URL{
 		Scheme: "https",
 		Host:   "accounts.google.com",
